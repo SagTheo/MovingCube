64fe8a27f163caf3fe255c6d6f1eea22e9bf1e4e
Theo <sagniez.theo@laposte.net>
2024-04-12 19:44:15 +0200

    functionality to make cube move in every direction works

--- scripts.js
+++ scripts.js
@@ -6,26 +6,26 @@ window.addEventListener('keydown', e => {
     if (e.code === 'ArrowRight') {
         x += 10
 
-        cube.style.transform = `translate(${x}px)`
+        cube.style.transform = `translate(${x}px, ${y}px)`
     }
 
     if (e.code === 'ArrowLeft') {
         x -= 10
 
-        cube.style.transform = `translate(${x}px)`
+        cube.style.transform = `translate(${x}px, ${y}px)`
     }
 
     if (e.code === 'ArrowUp') {
         y -= 10
 
         // needs more work
-        cube.style.transform = `translate(${x}, ${y}px)`
+        cube.style.transform = `translate(${x}px, ${y}px)`
     }
 
     if (e.code === 'ArrowDown') {
         y += 10
 
         // needs more work
-        cube.style.transform = `translate(${x}, ${y}px)`
+        cube.style.transform = `translate(${x}px, ${y}px)`
     }
 })
