6073a1875a280a8971d4695a379fe422fae5f3ff
Theo <sagniez.theo@laposte.net>
2024-04-13 11:43:31 +0200

    added line cube should move along + functionality to alert user when out of line

--- scripts.js
+++ scripts.js
@@ -1,31 +1,45 @@
 const cube = document.querySelector('.cube')
+const line = document.querySelector('.line')
+const lineCoord = line.getBoundingClientRect()
 let x = 0
 let y = 0
+const step = 10
+
+const transform = (cube, x, y) => {
+    cube.style.transform = `translate(${x}px, ${y}px)`
+}
 
 window.addEventListener('keydown', e => {
     if (e.code === 'ArrowRight') {
-        x += 10
+        x += step
 
-        cube.style.transform = `translate(${x}px, ${y}px)`
+        transform(cube, x, y)
     }
 
     if (e.code === 'ArrowLeft') {
-        x -= 10
+        x -= step
 
-        cube.style.transform = `translate(${x}px, ${y}px)`
+        transform(cube, x, y)
     }
 
     if (e.code === 'ArrowUp') {
-        y -= 10
+        y -= step
 
-        // needs more work
-        cube.style.transform = `translate(${x}px, ${y}px)`
+        transform(cube, x, y)
+
+        const cubeCoord = cube.getBoundingClientRect()
+
+        setTimeout(() => {
+            if (cubeCoord.top + 30 < lineCoord.top) {
+                alert('Out of line')
+            }
+        }, 100)
+        
     }
 
     if (e.code === 'ArrowDown') {
-        y += 10
+        y += step
 
-        // needs more work
-        cube.style.transform = `translate(${x}px, ${y}px)`
+        transform(cube, x, y)
     }
 })
